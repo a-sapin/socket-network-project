6b855a730bbc8c99a146cc6b64a38605db311e61
RainbowLoki <rainbowsuperloki@gmail.com>
2022-12-29 17:08:51 +0100

    Test program server works! :)

--- testProgs/testClient.c
+++ testProgs/testClient.c
@@ -12,18 +12,25 @@
 int main(int argc, char *argv[]) 
 {
 
+	if (argc!= 2)
+	{
+		perror("You need exactly one parameter (IP address of server network).");
+		exit(-1);
+	}
 	int dSock = socket(PF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in ad;
 	ad.sin_family = AF_INET;
 	ad.sin_addr.s_addr = INADDR_ANY;
+	socklen_t lgA = sizeof(struct sockaddr_in);
 
 
 	struct sockaddr_in adServ ;
 	adServ.sin_family = AF_INET; 
 	adServ.sin_port = htons(7777);
+	inet_pton(AF_INET, argv[1], &(adServ.sin_addr));
 
-	int res = inet_pton(AF_INET, ”197.50.51.10”, &(adServ.sin_addr));
-	socklen_t_lgA = sizeof(struct sockaddr_in);
-	res = connect(dS, (struct sockaddr *) &adServ, lgA);
+	//int res = inet_pton(AF_INET, ”197.50.51.10”, &(adServ.sin_addr));
+	//socklen_t_lgA = sizeof(struct sockaddr_in);
+	int res = connect(dSock, (struct sockaddr *) &adServ, lgA);
 
 }
--- testProgs/testServ.c
+++ testProgs/testServ.c
@@ -15,15 +15,30 @@ int main(int argc, char *argv[])
 	int dSock = socket(PF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in ad;
 	ad.sin_family = AF_INET;
-	ad.sin_addr.s_addr = INADDR_ANY;
+
+	if (argc == 1)
+	{
+		ad.sin_addr.s_addr = INADDR_ANY;
+	}
+	if (argc == 2)
+	{
+		inet_pton(AF_INET, argv[1], &(ad.sin_addr));
+
+	}
 	ad.sin_port = htons((short)7777);
+
+
 	int res = bind(dSock, (struct sockaddr*)&ad, sizeof(ad));
 	if(res==-1) perror("BINDING ERR ");
+
+	char str[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &(ad.sin_addr), str, INET_ADDRSTRLEN);
+	printf("Server is using IP %s \n", str);
 
 
 	//Listening now
 	int lis = listen(dSock, 1);
-	if (lis=!1)
+	if (lis==-1)
 	{
 		perror("LISTEN ERR ");
 	}
@@ -35,6 +50,7 @@ int main(int argc, char *argv[])
 
 	struct sockaddr_in adClient;
 	socklen_t lgA = sizeof(struct sockaddr_in);
+	printf("=Accept() starting now=\n");
 	int dSClient = accept(dSock, (struct sockaddr *) &adClient, &lgA);
 
 }
